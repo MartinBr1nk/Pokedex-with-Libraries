39596402efd516bea3f2d59e983574dc8c8e764d
MartinBr1nk <martin.brink@bcestudent.com>
2025-02-04 16:42:06 +0000

    Mykola is so cool
    Thanks

--- Chicken Chow MAIN.py
+++ Chicken Chow MAIN.py
@@ -1,4 +1,6 @@
 #import
+from idlelib.configdialog import help_pages
+
 import pandas as pd
 import matplotlib as plt
 from tkinter import *
@@ -6,6 +8,7 @@ from tkinter import ttk
 import csv
 import numpy as np
 import customtkinter as ctk
+
 
 #making a home page pretty much
 currentScene ="Main"
@@ -22,10 +25,10 @@ Button2 = '#8FAD88'
 SearchBar = '#CBDF90'
 
 def destroy_button():
-    for widget in buttonFrame.winfo_children():
+    for widget in buttonFrame.winfo_children(): # this destroys the buttons
         widget.destroy()
 
-def search_call():
+def search_call(): #search
     query = searchBar.get().strip().lower()
     filteredData = fileread[fileread["Name"].str.lower().str.contains(query)]
     destroy_button()
@@ -44,19 +47,53 @@ def search_call():
 
         pSearchName = filteredData["Name"].iloc[0]  # Get the 'Name' of the first entry for that count
         pSearchButton = ctk.CTkButton(buttonFrame, text=name, fg_color=Button2,
-                                      command=lambda tempName=name: poke_button_press(tempName))
+                                      command=lambda
+                                          name = name,
+                                          type1 = type1,
+                                          type2 = type2,
+                                          hp = hp,
+                                          atk = atk,
+                                          defense =defense,
+                                          spAtk = spAtk,
+                                          spDef = spDef,
+                                          spd = spd,
+                                          gen = gen,
+                                          leg = leg:
+                                          poke_button_press(name, type1, type2, hp, atk, defense, spAtk, spDef, spd, gen, leg))
+
+
+
+                                    #tempName=name: poke_button_press(tempName))
         pSearchButton.pack(pady=5, fill="both", expand=True)
 
 
-def poke_button_press(name):
-    app= ctk.CTk()
+def poke_button_press(name, type1, type2, hp, atk, defense, spAtk, spDef, spd, gen, leg):
+    dataFrame.delete("1.0", "end") #clears the frame so new stuff can appear
+
+#this is text for the buttons for the pokemon so the pokemon work 
+    dataFrame.insert("1.0",
+                    f"name: {name}"
+                     f"\nType 1: {type1}"
+                     f"\nType 2: {type2}"
+                     f"\nHP: {hp}"
+                     f"\nAttack: {atk}"
+                     f"\nDefense: {defense}"
+                     f"\nSp. Atk: {spAtk}"
+                     f"\nSp. Def: {spDef}"
+                     f"\nSpeed: {spd}"
+                     f"\nGeneration: {gen}"
+                     f"\nLegendary: {leg}")
+
+
 
 app = ctk.CTk(fg_color=Wall1)
 app.geometry("500x900")
 app.title("Pokedex with added storm flute")
 app.resizable(width = False, height = False)
 
-
+dataFrame = ctk.CTkTextbox(app)
+pokestats = dataFrame.insert(0.0,"Pokemon Stats Go Here!")
+dataFrame.pack(side = "right", fill="y")
 
 searchFrame = ctk.CTkFrame(app, fg_color= Wall1)
 searchFrame.pack(fill= "x")
